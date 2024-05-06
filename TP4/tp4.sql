f7560e2612af3138e573acd01d24daa8ca9387eb
BohBOhTN <90150981+BohBOhTN@users.noreply.github.com>
2024-05-06 12:29:24 +0100

    TP n°4 : Architecture de Base de données sous Oracle

---
+++ TP4/tp4.sql
@@ -0,0 +1,41 @@
+----------------TP n°4 : Architecture de Base de données sous Oracle--------------------------------------
+
+/*
+
+Question 2
+Le SGA est une zone de mémoire partagée utilisée par la base de données 
+pour stocker des données et des informations de contrôle telles que le 
+cache de base de données, le cache de redémarrage, le cache de 
+dictionnaire, le cache de curseur partagé, etc.
+
+
+
+Cette commande affiche les paramètres de configuration liés à la gestion 
+de la mémoire du SGA (System Global Area) dans Oracle Database.
+
+SGA SYSTEM GLOBAL AREA 
+PGA PROGRAM GLOBAL AREA
+*/
+
+
+-- Question 3
+
+SHOW USER;
+
+
+-- Question 4
+
+SHOW PARAMETER SESSIONS;
+
+-- QUESTION 5
+
+SHOW PARAMETER PROCESSES;
+
+-- QUESTION 6
+
+SELECT INSTANCE_NAME , STARTUP_TIME FROM V$INSTANCE;
+
+/*
+CETTE COMMANDE AFFICHE LE NOM DE L'INSTANCE ET LE DATE DE DEMMARAGE
+*/
+
